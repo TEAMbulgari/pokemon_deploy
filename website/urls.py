97784689adbd245566ba60203f8d7934190adcc2
Minjeong <mathmjseo@naver.com>
2020-11-23 00:04:46 +0900

    Test

--- website/urls.py
+++ website/urls.py
@@ -29,4 +29,4 @@ urlpatterns = [
     path('canvasToImage/', pageView.canvasToImage, name='canvasToImage'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, insecure=True)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
